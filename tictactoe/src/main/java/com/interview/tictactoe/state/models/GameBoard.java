80fe6c6b1da6c78a8bf60cd3b67fa5604e7ab947
steve hiehn <stevehiehn@gmail.com>
2019-01-12 14:24:17 -0800

    added tests for board mechanics

--- tictactoe/src/main/java/com/interview/tictactoe/state/models/GameBoard.java
+++ tictactoe/src/main/java/com/interview/tictactoe/state/models/GameBoard.java
@@ -1,6 +1,7 @@
 package com.interview.tictactoe.state.models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -32,6 +33,59 @@ public class GameBoard implements Serializable {
             throw new RuntimeException("unsupported location");
         }
 
-        return getBoard();
+        if(this.getBoard().get(position) != null) {
+            return this.getBoard();
+        }
+
+        this.getBoard().put(position, player);
+
+        return this.getBoard();
+    }
+
+    public String checkForWinner(){
+        for(List<String> combos : getWinningCombos()){
+
+            String winner = getBoard().get(combos.get(0));
+                if (getBoard().get(combos.get(0)) != null &&
+                        getBoard().get(combos.get(1)) != null &&
+                        getBoard().get(combos.get(2)) != null) {
+
+                    if (getBoard().get(combos.get(0)).equalsIgnoreCase(winner) &&
+                            getBoard().get(combos.get(1)).equalsIgnoreCase(winner) &&
+                            getBoard().get(combos.get(2)).equalsIgnoreCase(winner)) {
+                        return winner;
+                    }
+                }
+        }
+
+        return null;
+    }
+
+    private List<List<String>> getWinningCombos(){
+        List<List<String>> winningCombos = new ArrayList<>();
+
+        List<String> rowA = Arrays.asList("a0","a1","a2");
+        List<String> rowB = Arrays.asList("b0","b1","b2");
+        List<String> rowC = Arrays.asList("c0","c1","c2");
+
+        List<String> col1 = Arrays.asList("a0","b0","c0");
+        List<String> col2 = Arrays.asList("a1","b1","c1");
+        List<String> col3 = Arrays.asList("a2","b2","c2");
+
+        List<String> diagonal1 = Arrays.asList("a0","b1","c2");
+        List<String> diagonal2 = Arrays.asList("a2","b1","c0");
+
+        winningCombos.add(rowA);
+        winningCombos.add(rowB);
+        winningCombos.add(rowC);
+
+        winningCombos.add(col1);
+        winningCombos.add(col2);
+        winningCombos.add(col3);
+
+        winningCombos.add(diagonal1);
+        winningCombos.add(diagonal2);
+
+        return winningCombos;
     }
 }
--- tictactoe/src/test/java/com/interview/tictactoe/state/models/GameBoardTest.java
+++ tictactoe/src/test/java/com/interview/tictactoe/state/models/GameBoardTest.java
@@ -3,28 +3,96 @@ package com.interview.tictactoe.state.models;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameBoardTest {
 
     GameBoard board;
 
-    @Before
     public void setup(){
         board = new GameBoard();
     }
 
     @Test(expected = RuntimeException.class)
     public void throwsErrorWhenPositionNotSupported(){
+        setup();
+
         board.update("fake", "bad position");
     }
 
-    public void returnsBoardWhenPositionUpdated(){
-        board.update("fake", "bad position");
+    @Test
+    public void updatesBoardWhenPositionIsValid(){
+        setup();
+
+        assertThat(board.getBoard().get("a2")).isNull();
+
+        HashMap<String, String> updatedBoard = board.update("playerA", "a2");
+        assertThat(updatedBoard.get("a2")).isNotNull();
+        assertThat(updatedBoard.get("a2")).isEqualToIgnoringCase("playerA");
     }
 
-    // must be correct players move
+    @Test
+    public void doesNotUpdateBoardWhenPositionAlreadyTaken(){
+        setup();
+
+        assertThat(board.getBoard().put("a2", "playerB")).isNull();
+
+        HashMap<String, String> updatedBoard = board.update("playerA", "a2");
+        assertThat(updatedBoard.get("a2")).isNotNull();
+        assertThat(updatedBoard.get("a2")).isEqualToIgnoringCase("playerB");
+    }
+
+    @Test
+    public void returnsNullWhenNoWinner(){
+        setup();
+
+        board.getBoard().put("c0", "playerA");
+        board.getBoard().put("c1", "playerB");
+        board.getBoard().put("c2", "playerB");
+
+        assertThat(this.board.checkForWinner()).isNull();
+    }
+
+    @Test
+    public void returnsWinnerDiagonal(){
+        setup();
+
+        board.getBoard().put("c0", "playerA");
+        board.getBoard().put("b1", "playerA");
+        board.getBoard().put("a2", "playerA");
+
+        board.getBoard().put("c1", "otherPlayer");
+
+        assertThat(this.board.checkForWinner()).isEqualToIgnoringCase("playerA");
+    }
+
+    @Test
+    public void returnsWinnerRow(){
+        setup();
+
+        board.getBoard().put("b0", "playerB");
+        board.getBoard().put("b1", "playerB");
+        board.getBoard().put("b2", "playerB");
+
+        board.getBoard().put("c1", "otherPlayer");
+
+        assertThat(this.board.checkForWinner()).isEqualToIgnoringCase("playerB");
+    }
+
+    @Test
+    public void returnsWinnerCol(){
+        setup();
+
+        board.getBoard().put("c0", "playerC");
+        board.getBoard().put("c1", "playerC");
+        board.getBoard().put("c2", "playerC");
+
+        board.getBoard().put("a1", "otherPlayer");
+
+        assertThat(this.board.checkForWinner()).isEqualToIgnoringCase("playerC");
+    }
 
 
-    // check for 8 winning combos
 }
